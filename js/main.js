6ce12d9a2ed742cab77e7391feaf27b5e89395b5
javierspinoza <jespinosa937@misena.edu.co>
2020-09-04 17:24:07 -0500

    inicie el metodo ensamblador

--- js/main.js
+++ js/main.js
@@ -52,7 +52,9 @@ var app = new Vue({
                 this.totalE = parseInt(this.horaE) * parseInt(this.cant1);
             }
         },
+        ensamblador:function(){
             
+        }  
         
     }       
 })
