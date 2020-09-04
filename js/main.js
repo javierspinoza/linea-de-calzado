0ac352c6ade04905fb0175440d31a1431617b20b
javierspinoza <jespinosa937@misena.edu.co>
2020-09-04 17:10:30 -0500

    realize la password y lo del secretario

--- js/main.js
+++ js/main.js
@@ -9,28 +9,50 @@ var app = new Vue({
         cont2:null,
         cont3:null,
         cont4:null,
+        msj:"",
 
         nom1:null,
         nom2:null,
         nom3:null,
 
-        cant1:0,
-        cant2:0,
-        cant3:0,
-        cant4:0,
-        cant5:0,
+        cant1:null,
+        cant2:null,
+        cant3:null,
+        cant4:null,
+        cant5:null,
 
-        valor1:0,
+        valor1:0
         
-
-
+        
     },
     methods: {
         password:function(){
+            if(this.cont1== "5487"){
 
+            }
+            else if(this.cont2== "1234"){
+
+            }
+            if(this.cont3== "1233"){
+            
+            }
+            else if(this.cont4== "5334"){
+
+            }
         },
+
         procesar:function() {
-        }    
+            if(this.tipo == "2"){
+                this.salarioB = 857000;
+                this.vHora = 52000;
+                this.msj == "Secretario";
+                
+
+                this.vHora = parseInt(this.vHora) * (180/100);
+                this.totalE = parseInt(this.horaE) * parseInt(this.cant1);
+            }
+        },
+            
         
     }       
 })
