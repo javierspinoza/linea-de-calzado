0ee9ea6e56362d97118665bc570f798c154e2528
javierspinoza <jespinosa937@misena.edu.co>
2020-09-04 19:55:08 -0500

    deje ya terminado el metodo procesar y cree dos metodos mas para empezar a trabajarlos

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ var app = new Vue({
         cont2:null,
         cont3:null,
         cont4:null,
-        msj:"",
+        
 
         nom1:null,
         nom2:null,
@@ -20,7 +20,8 @@ var app = new Vue({
         cant3:null,
         cant4:null,
         cant5:null,
-
+        total:0,
+        totalEx:0,
         valor1:0
         
         
@@ -42,19 +43,34 @@ var app = new Vue({
         },
 
         procesar:function() {
-            if(this.tipo == "2"){
+            if(this.cant1 < 0){
+                this.mensaje("datos incorretos","error")
+            }
+             if(this.tipo == "2"){
                 this.salarioB = 857000;
                 this.vHora = 52000;
-                this.msj == "Secretario";
+                this.msj = "Secretario";
                 
-
-                this.vHora = parseInt(this.vHora) * (180/100);
-                this.totalE = parseInt(this.horaE) * parseInt(this.cant1);
+                this.vHoraT = parseInt(this.vHora) * (180/100);
+                this.totalEx =  parseInt(this.cant1) * parseInt(this.vHoraT)
+                this.total = parseInt(this.salarioB) + parseInt(this.totalEx);
             }
         },
+        vendedor:function(){
+
+        },
+
         ensamblador:function(){
             
-        }  
-        
+        }, 
+        mensaje: function (msj, icono) {
+            Swal.fire({
+            position: 'top-end',
+            icon: icono,
+            title: msj,
+            showConfirmButton: false,
+            timer: 1500
+            })
+        },
     }       
 })
