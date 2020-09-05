aa0d602396d4663610115dfe7d57f2f487049dfb
javierspinoza <jespinosa937@misena.edu.co>
2020-09-05 10:57:15 -0500

    actualice el archivo

--- js/main.js
+++ js/main.js
@@ -1,149 +1,89 @@
 var app = new Vue({
-<<<<<<< HEAD
-    el: '#app',
-    data: {
-
+    el:'#app',
+    data:{
+    
         arrayDatos: [],
+        
+        tipo:0,
+        ver:0,
+        cont1:null,
+        cont2:null,
+        cont3:null,
+        cont4:null,
+        
 
-        tipo: 0,
-        cont1: null,
-        cont2: null,
-        cont3: null,
-        cont4: null,
+        nom1:null,
+        nom2:null,
+        nom3:null,
 
-
-        nom1: null,
-        nom2: null,
-        nom3: null,
-
-        cant1: null,
-        cant2: null,
-        cant3: null,
-        cant4: null,
-        cant5: null,
-        total: 0,
-        totalEx: 0,
-        valor1: 0
-
-
+        cant1:null,
+        cant2:null,
+        cant3:null,
+        cant4:null,
+        cant5:null,
+        total:0,
+        totalEx:0,
+        valor1:0
+        
+        
     },
     methods: {
-        password: function () {
-            if (this.cont1 == "5487") {
+        password:function(){
+            // if(this.cont1== "5487"){
+        
+            // }
+            if(this.tipo== "2"){
+                if (this.cont2=="1234") {
+                    this.ver=2;
+                    this.secretario();
+                }else{
+                    this.mensaje("error en la clave","error");
+                }
 
             }
-            else if (this.cont2 == "1234") {
+            // if(this.cont3== "1233"){
+            
+            // }
+            // else if(this.cont4== "5334"){
 
-            }
-            if (this.cont3 == "1233") {
-
-            }
-            else if (this.cont4 == "5334") {
-=======
-  el: "#app",
-  data: {
-    arrayDatos: [],
-
-    tipo: 0,
-    cont1: null,
-    cont2: null,
-    cont3: null,
-    cont4: null,
-
-    nom1: null,
-    nom2: null,
-    nom3: null,
-
-    cant1: null,
-    cant2: null,
-    cant3: null,
-    cant4: null,
-    cant5: null,
-    total: 0,
-    totalEx: 0,
-    valor1: 0,
-  },
-  methods: {
-    password: function () {
-      if (this.cont1 == "5487") {
-      } else if (this.cont2 == "1234") {
-      }
-      if (this.cont3 == "1233") {
-      } else if (this.cont4 == "5334") {
-      }
-    },
->>>>>>> 71b1021aef9d0792caff940f9792f627bd5fb4eb
-
-    procesar: function () {
-      if (this.cant1 < 0) {
-        this.mensaje("datos incorretos", "error");
-      }
-      if (this.tipo == "2") {
-        this.salarioB = 857000;
-        this.vHora = 52000;
-        this.msj = "Secretario";
-
-<<<<<<< HEAD
-        procesar: function () {
-            if (this.cant1 < 0) {
-                this.msj("datos incorretos", "error")                
-            }
-            else if (this.tipo == "2") {
-                this.salarioB = 857000;
-                this.vHora = 52000;
-                this.msj = "Secretario";
-
-                this.vHoraT = parseInt(this.vHora) * (180 / 100);
-                this.totalEx = parseInt(this.cant1) * parseInt(this.vHoraT)
-                this.total = parseInt(this.salarioB) + parseInt(this.totalEx);
-            }
-        },
-        javier: function (){
-            alert("holajavier")
+            // }
         },
 
-        vendedor: function () {
+        procesar:function() {
+            
+            if(this.tipo == "1"){
+                alert("administrador");
+            }else if(this.tipo == "2"){
+                this.password("2",this.cont1);
+                this.secretario();
+            }if(this.tipo == "3"){
+                alert("vendedor");
 
-        },        
-
-        ensamblador: function () {
-
+            }
+        },
+         
+        secretario(){
+            if (this.cant1<0) {
+                this.mensaje("Horas NO validas ", "error");
+            }else{
+            this.salarioB = 857000;
+            this.vHora = 52000;
+            this.msj = "Secretario";
+            
+            this.vHoraT = parseInt(this.vHora) * (180/100);
+            this.totalEx =  parseInt(this.cant1) * parseInt(this.vHoraT)
+            this.total = parseInt(this.salarioB) + parseInt(this.totalEx);
+            }
         },
         mensaje: function (msj, icono) {
             Swal.fire({
-                position: 'top-end',
-                icon: icono,
-                title: msj,
-                showConfirmButton: false,
-                timer: 1500
+            position: 'top-end',
+            icon: icono,
+            title: msj,
+            showConfirmButton: false,
+            timer: 1500
             })
         },
-    }
+    }       
 })
-=======
-        this.vHoraT = parseInt(this.vHora) * (180 / 100);
-        this.totalEx = parseInt(this.cant1) * parseInt(this.vHoraT);
-        this.total = parseInt(this.salarioB) + parseInt(this.totalEx);
-      }
-    },
-    ozkr(){
-        alert("Hola mundo");
-    },
-    otroDemo(){
 
-    },
-    vendedor: function () {},
-
-    ensamblador: function () {},
-    mensaje: function (msj, icono) {
-      Swal.fire({
-        position: "top-end",
-        icon: icono,
-        title: msj,
-        showConfirmButton: false,
-        timer: 1500,
-      });
-    },
-  },
-});
->>>>>>> 71b1021aef9d0792caff940f9792f627bd5fb4eb
